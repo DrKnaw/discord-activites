c2a64c37ded014fe0124292d3537cc790886cd0f
Esteban <internet.drknaw@gmail.com>
2021-05-13 06:53:05 +0200

    Create discord_activities.sh

---
+++ discord_activities.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo ""
+echo " _____  _                       _                _   _       _ _   _           "
+echo "|  __ \(_)                     | |     /\       | | (_)     (_) | (_)          "
+echo "| |  | |_ ___  ___ ___  _ __ __| |    /  \   ___| |_ ___   ___| |_ _  ___  ___ "
+echo "| |  | | / __|/ __/ _ \|  __/ _  |   / /\ \ / __| __| \ \ / / | __| |/ _ \/ __|"
+echo "| |__| | \__ \ (_| (_) | | | (_| |  / ____ \ (__| |_| |\ V /| | |_| |  __/\__  "
+echo "|_____/|_|___/\___\___/|_|  \__ _| /_/    \_\___|\__|_| \_/ |_|\__|_|\___||___/"
+echo ""
+echo ""
+echo "Enter a discord token (silent input):"
+read -s
+if [ ${#REPLY} = "59" ]
+then
+        echo ""
+        TOKEN=$REPLY
+else
+        echo ""
+        echo "Invalid token."
+        exit
+fi
+echo "Wich activity do you want?"
+select yn in "YouTube" "Poker Night" "Betrayal" "Fishington" "Exit"; do
+    case $yn in
+        YouTube ) APP_ID=755600276941176913; break;;
+        Poker\ Night ) echo ''; echo 'Not Implemented yet';;
+        Betrayal ) echo ''; echo 'Not Implemented yet';;
+        Fishington ) echo ''; echo 'Not Implemented yet';;
+        Other ) echo ''; echo 'Not Implemented yet';;
+        Exit ) exit;;
+    esac
+done
+
+
+generate_post_data()
+{
+  cat <<EOF
+{"target_application_id":"$APP_ID","target_type":"2"}
+EOF
+}
+echo ""
+echo "Enter the ID of the voice channel:"
+read
+CHANNEL_ID=$REPLY
+echo "Fetching Discord API..."
+echo ""
+RESULT=`curl --silent -X POST -H "Authorization: $TOKEN" --data "$(generate_post_data)" -H "Content-Type: application/json" https://discord.com/api/v8/channels/$CHANNEL_ID/invites | grep code | awk {'print $2'} | sed 's/"//g' | sed 's/,//g'`
+echo "Fetching Done."
+echo ""
+echo "The Link to join the activity is the following:"
+echo ""
+echo "https://discord.gg/"$RESULT
+echo ""
+echo "Script Made by DrKnaw (https://drknaw.eu)"
