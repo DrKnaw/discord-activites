e15fc3431c46efa1cc6994c7fc8e337882dba976
Esteban <internet.drknaw@gmail.com>
2021-05-17 20:34:26 +0200

    AmogUS

--- discord_activities.sh
+++ discord_activities.sh
@@ -8,24 +8,51 @@ echo "| |__| | \__ \ (_| (_) | | | (_| |  / ____ \ (__| |_| |\ V /| | |_| |  __/
 echo "|_____/|_|___/\___\___/|_|  \__ _| /_/    \_\___|\__|_| \_/ |_|\__|_|\___||___/"
 echo ""
 echo ""
-echo "Enter a discord token (silent input):"
-read -s
-if [ ${#REPLY} = "59" ]
-then
+if [ -f "./token.txt" ]; then
+   while read line; do 
+   if [ ${#line} = "59" ]; then
         echo ""
-        TOKEN=$REPLY
+        TOKEN=$line
+else
+        echo ""
+        echo "Invalid token. (if persit try removing token.txt)"
+        exit
+fi
+  
+  
+  done < token.txt
+  else
+   echo "Enter a discord token (silent input):"
+  read -s token
+     if [ ${#token} = "59" ]; then
+        echo ""
+        TOKEN=$token
 else
         echo ""
         echo "Invalid token."
         exit
 fi
+  
+  echo "Do you want to save the token ? (y or n)"
+  read ouinon
+  if echo "$ouinon" | grep -iq "^y" ;then
+ echo $token >> token.txt
+ else 
+ echo "Token not save."
+ echo ""
+ fi
+
+
+fi
+
+
 echo "Wich activity do you want?"
-select yn in "YouTube" "Poker Night" "Betrayal" "Fishington" "Other" "Exit"; do
-    case $yn in
+select activites in "YouTube" "Poker Night" "Betrayal" "Fishington" "Other" "Exit"; do
+    case $activites in
         YouTube ) APP_ID=755600276941176913; break;;
         Poker\ Night )  APP_ID=755827207812677713;  break;;
         Betrayal ) APP_ID=755827207812677713;  break;;
-        Fishington )  echo ''; echo 'Not Implemented Yet';;
+        Fishington )  APP_ID=814288819477020702; break;;
         Other ) echo ''; echo 'Enter the ID of the Application:'; read; APP_ID=$REPLY; break;;
         Exit ) exit;;
     esac
@@ -43,10 +70,15 @@ echo "Enter the ID of the voice channel:"
 read
 CHANNEL_ID=$REPLY
 echo "Fetching Discord API..."
+echo "Bot Token or User Token ?"
+select botoruser in "Bot" "User"; do
+    case $botoruser in
+        Bot ) RESULT=`curl --silent -X POST -H "Authorization: Bot $TOKEN" --data "$(generate_post_data)" -H "Content-Type: application/json" https://discord.com/api/v8/channels/$CHANNEL_ID/invites | grep code | awk {'print $2'} | sed 's/"//g' | sed 's/,//g'`; break;;
+        User ) RESULT=`curl --silent -X POST -H "Authorization: $TOKEN" --data "$(generate_post_data)" -H "Content-Type: application/json" https://discord.com/api/v8/channels/$CHANNEL_ID/invites | grep code | awk {'print $2'} | sed 's/"//g' | sed 's/,//g'`;  break;;
+    esac
+done
 echo ""
-RESULT=`curl --silent -X POST -H "Authorization: $TOKEN" --data "$(generate_post_data)" -H "Content-Type: application/json" https://discord.com/api/v8/channels/$CHANNEL_ID/invites | grep code | awk {'print $2'} | sed 's/"//g' | sed 's/,//g'`
 echo "Fetching Done."
-echo ""
 echo "The Link to join the activity is the following:"
 echo ""
 echo "https://discord.gg/"$RESULT
